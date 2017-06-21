eec25fda41f49c6e3c26a86c43fb5bd0f5252862
Lou <louisarg@live.com>
2017-06-20 17:18:28 -0700

    Plane icon

--- GoogleMapsAPI/ViewController.swift
+++ GoogleMapsAPI/ViewController.swift
@@ -13,6 +13,7 @@ import GooglePlaces
 
 class ViewController: UIViewController {
 
+    
     override func loadView() {
         
     }
@@ -25,6 +26,40 @@ class ViewController: UIViewController {
         let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
+        print("loading...")
+        
+        let req = OpenSkyRequest()
+        req.fetch(coordination: [49.2827, -123.1207]) {data, res, err in
+            if err != nil {
+                print("ERR \(err!)")
+                return
+            }
+            
+            let queue = DispatchQueue.main
+            
+            queue.async {
+            
+                print("There are \(data?.count ?? 0) airplane(s) near here!")
+                for s in data! {
+                    self.printState(s)
+                    let planePosition = CLLocationCoordinate2D(latitude: s.latitude!, longitude: s.longitude!)
+                    let newFlight = GMSMarker(position: planePosition)
+                    newFlight.title = s.icao24
+                    newFlight.icon = UIImage(named: "Plane1")
+                    newFlight.map = mapView
+                    
+                    print("the \(newFlight) is here")
+                }
+            }
+            
+            
+        }
+        
+        if let mylocation = mapView.myLocation {
+            print("User's location: \(mylocation)")
+        } else {
+            print("User's location is unknown")
+        }
         
         //          Creates a marker in the center of the map.
         let marker = GMSMarker()
@@ -33,7 +68,7 @@ class ViewController: UIViewController {
         marker.snippet = "Canada"
         marker.map = mapView
         navigationItem.title = "Hello VanFlight"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Print", style: .plain, target: self, action: #selector(test))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Request Info", style: .plain, target: self, action: #selector(test))
         //        create compass button
         mapView.settings.compassButton = true
         
@@ -46,18 +81,25 @@ class ViewController: UIViewController {
     func test() {
         print("loading...")
         
-        let req = OpenSkyRequest()
-        req.fetch(coordination: [49.2827, -123.1207]) {data, res, err in
-            if err != nil {
-                print("ERR \(err!)")
-                return
-            }
-            
-            print("There are \(data?.count ?? 0) airplane(s) near here!")
-            for s in data! {
-                self.printState(s)
-            }
-        }
+//        let req = OpenSkyRequest()
+//        req.fetch(coordination: [49.2827, -123.1207]) {data, res, err in
+//            if err != nil {
+//                print("ERR \(err!)")
+//                return
+//            }
+//            
+//            print("There are \(data?.count ?? 0) airplane(s) near here!")
+//            for s in data! {
+//                self.printState(s)
+//                let planePosition = CLLocationCoordinate2D(latitude: s.latitude!, longitude: s.longitude!)
+//                let newFlight = GMSMarker(position: planePosition)
+//                newFlight.title = s.icao24
+//                newFlight.icon = UIImage(named: "1498012390_one_way.png")
+//                newFlight.map = GMSMapView()
+//                                
+//                print("the \(newFlight) is here")
+//            }
+//        }
     }
 
     
