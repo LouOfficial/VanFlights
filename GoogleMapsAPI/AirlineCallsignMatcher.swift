f5c89f5c8e49665dc9bb6f311b067bd3ae88dbdb
ginpei <ginpei@gmail.com>
2017-06-20 17:03:49 -0700

    find airline name

---
+++ GoogleMapsAPI/AirlineCallsignMatcher.swift
@@ -0,0 +1,64 @@
+//
+//  AirlineCallsignMatcher.swift
+//  GoogleMapsAPI
+//
+//  Created by Ginpei on 2017-06-20.
+//  Copyright © 2017 Luis Alfredo Ramirez. All rights reserved.
+//
+
+import Foundation
+
+class AirlineCallsignMatcher {
+    static let dictionary = [
+        "AAL": "American Airlines",
+        "AAR": "Asiana Airlines",
+        "ABW": "AirBridge Cargo",
+        "ANA": "All Nippon Airways",
+        "ASA": "Alaska Airlines, Inc.",
+        "BAW": "British Airways",
+        "BOE": "Boeing",
+        "BYF": "San Carlos Flight Center",
+        "CCA": "Air China",
+        "CES": "China Eastern Airlines",
+        "CFE": "BA CityFlyer",
+        "CGE": "Nelson Aviation College",
+        "CGX": "United States Coast Guard Auxiliary",
+        "CHH": "Hainan Airlines",
+        "CKK": "China Cargo Airlines",
+        "CPZ": "Compass Airlines",
+        "DAL": "Delta Air Lines",
+        "DLH": "Lufthansa",
+        "EJA": "NetJets",
+        "EVA": "EVA Air",
+        "FDX": "Federal Express",
+        "FIN": "Finnair",
+        "ICE": "Icelandair",
+        "JAL": "Japan Airlines",
+        "KAL": "Korean Air",
+        "KLM": "KLM",
+        "PAL": "Philippine Airlines",
+        "PCM": "Westair Industries",
+        "SKW": "SkyWest Airlines",
+        "SWA": "Southwest Airlines",
+        "SWR": "China Eastern Airlines",
+        "THY": "Turkish Airlines",
+        "TSC": "Air Transat",
+        "UAL": "United Airlines",
+        "UPS": "United Parcel Service",
+        "VIR": "Virgin Atlantic Airways",
+        "VRD": "Virgin America",
+        "WJA": "WestJet",
+        ]
+    
+    static func findBy(callsign: String) -> String? {
+        if callsign.characters.count > 3 {
+            let index = callsign.index(callsign.startIndex, offsetBy: 3)
+            let prefix = callsign.substring(to: index)
+            if let name = dictionary[prefix] {
+                return name
+            }
+        }
+        
+        return nil
+    }
+}
